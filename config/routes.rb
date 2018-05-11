b08090cd7388a42d012939fcc8a2ecfaaf42c846
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-11 15:41:56 +0800

    Can't seem to open any pages visually

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "articles", to: "articles#index", as: "articles"
+
+  get "articles/new", to: "articles#new", as: "new_article"
+  post "articles", to: "articles#create", as: "create_article"
+
+  get "articles/:id", to: "articles#show", as: "article"
+
+  get "articles/:id/edit", to: "articles#edit", as: "edit_article"
+  post "articles/:id", to: "articles#update"
+
+  get "articles/:id", to: "articles#detroy", as: "destroy_article"
 end
